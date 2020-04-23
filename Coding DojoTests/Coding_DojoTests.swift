81279e1f4821e06a2e6f76e2ee9b104ebe92f5ac
Pongsakorn Onsri <pongsakorn@central.tech>
2020-04-23 23:50:20 +0700

    starting point

--- Coding DojoTests/Coding_DojoTests.swift
+++ Coding DojoTests/Coding_DojoTests.swift
@@ -18,9 +18,6 @@ class Coding_DojoTests: QuickSpec {
             context("if thow dice") {
                 it("got value 2,2,3,4,6,6") {
                     let values = [2,2,3,4,6,6]
-                    let dice = Dice(values: values)
-                    let score = dice.score()
-                    expect(score).to(equal(0))
                 }
             }
         }

3d5f83e71a8b02a19df56b4b04bffbb820ae5628
Tangkwa <juthamas@central.tech>
2020-05-08 19:35:05 +0700

    case button2

--- Sources/coding-dojo/Phone.swift
+++ Sources/coding-dojo/Phone.swift
@@ -9,13 +9,34 @@ import Foundation
 
 class Phone {
     
-    let input: String
+    var input: String
+    
+    var button1: [String] = [",", ",", "/"]
+    var button2: [String] = ["A", "B", "C"]
+    var button3: [String] = ["D", "E", "F"]
+    var button4: [String] = ["G", "H", "I"]
+    var button5: [String] = ["J", "K", "L"]
+    var button6: [String] = ["M", "N", "O"]
+    var button7: [String] = ["P", "Q", "R", "S"]
+    var button8: [String] = ["T", "U", "V"]
+    var button9: [String] = ["W", "X", "Y", "Z"]
+    var button0: [String] = ["+"]
+    var buttonStar: (() -> Void)? = nil
+    var buttonShap: (() -> Void)? = nil
     
     init(input: String) {
         self.input = input
     }
     
     func getMessage() -> String {
-        return input
+        var output: String = ""
+        if input == "2" {
+            output = button2.first ?? ""
+        } else if input == "22" {
+            output = button2[1]
+        } else if input == "222" {
+            output = button2[2]
+        } 
+        return output
     }
 }
--- Tests/coding-dojoTests/PhoneTests.swift
+++ Tests/coding-dojoTests/PhoneTests.swift
@@ -16,16 +16,35 @@ final class Phone_Test : QuickSpec {
         
         describe("As a Phone") {
             
-            it("input is 112345 then message is 112345") {
-                let phone = Phone(input: "112345")
-                let message = phone.getMessage()
-                expect(message).to(be("112345"))
-            }
-            
-            it("input is 11 then message is A") {
-                let phone = Phone(input: "11")
+            it("input is 2 then message is A") {
+                let phone = Phone(input: "2")
                 let message = phone.getMessage()
                 expect(message).to(be("A"))
+            }
+            it("input is 22 then message is B") {
+                let phone = Phone(input: "22")
+                let message = phone.getMessage()
+                expect(message).to(be("B"))
+            }
+            it("input is 222 then message is C") {
+                let phone = Phone(input: "222")
+                let message = phone.getMessage()
+                expect(message).to(be("C"))
+            }
+            it("input is 3 then message is D") {
+                let phone = Phone(input: "3")
+                let message = phone.getMessage()
+                expect(message).to(be("D"))
+            }
+            it("input is 33 then message is E") {
+                let phone = Phone(input: "33")
+                let message = phone.getMessage()
+                expect(message).to(be("E"))
+            }
+            it("input is 333 then message is F") {
+                let phone = Phone(input: "333")
+                let message = phone.getMessage()
+                expect(message).to(be("F"))
             }
         }
     }
